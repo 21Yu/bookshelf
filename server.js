308da03fc4d4a83d5e1c18999b07377fcaf24d66
12Yu <12yudev@gmail.com>
2024-06-02 19:51:03 -0700

    not sure it will work

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -11,9 +15,8 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {
-useNewUrlParser: true })
-const db = mongoose.connction
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
