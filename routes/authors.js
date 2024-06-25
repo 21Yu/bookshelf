780ccf0972f15211d9f8557e711545a2cb775c89
12Yu <12yudev@gmail.com>
2024-06-25 11:07:40 -0700

    error message and async await

--- routes/authors.js
+++ routes/authors.js
@@ -10,20 +10,29 @@ router.get('/new', (req, res) => {
     res.render('authors/new', { author: new Author() })
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name
     })
-    author.save().
-    then((newAuthor)=>{
-        res.render('authors')
-    }).
-    catch((err)=>{
+    try {
+        const newAuthor = await author.save()
+        res.redirect('authors')
+    } catch {
         res.render('authors/new',{
             author: author,
-            errMessage:'Error Creating Author...'
-        })
-    })
+            errorMessage:'Error Creating Author...'
+        }) 
+    }
+    // author.save().
+    // then((newAuthor)=>{
+    //     res.render('authors')
+    // }).
+    // catch((err)=>{
+    //     res.render('authors/new',{
+    //         author: author,
+    //         errorMessage:'Error Creating Author...'
+    //     })
+    // })
     
 })
 module.exports = router
