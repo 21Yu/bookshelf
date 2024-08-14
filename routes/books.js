a186a4d69e85c2bb12352fb14f0beec631a06e5c
12Yu <12yudev@gmail.com>
2024-08-14 13:46:48 -0700

    render new page

--- routes/books.js
+++ routes/books.js
@@ -2,26 +2,56 @@ const express = require('express')
 const router = express.Router()
 const Book = require('../models/book')
 const Author = require('../models/author')
-
+const path = require('path')
+const uploadPath = path.join('public', Book.coverImageBasePath)
+const multer = require('multer')
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
+const upload = multer({
+    dest: uploadPath,
+    fileFilter: (req, file, callback) => {
+        callback(null, imageMimeTypes.includes(file.mimetype))
+    }
+})
 
 router.get('/', async (req, res) => {
     res.send('All books')
 })
 
 router.get('/new', async (req, res) => {
+    renderNewPage(res, new Book())
+})
+
+router.post('/', upload.single('cover'), async (req, res) => {
+    const fileName = req.file != null ? req.file.filename : null
+    const book = new Book ({
+        title: req.body.title,
+        author: req.body.author,
+        publishDate: new Date(req.body.publishDate),
+        pageCount: req.body.pageCount,
+        coverImageName: fileName,
+        description: req.body.description
+    })
+
     try {
-        const authors = await Author.find({})
-        const book = new Book()
-        res.render('books/new', {
-            authors: authors,
-            book: book
-        })
+        const newBook = await book.save()
+        // res.redirect(`books/${newBook.id}`)
+        res.redirect('books')
     } catch {
-        res.redirect('/books')
+        renderNewPage(res, book, true)
     }
 })
 
-router.post('/', async (req, res) => {
-    res.send('Create book')
-})
+async function renderNewPage(res, book, hasError = false) {
+    try {
+        const authors = await Author.find({})
+        const params = {
+            authors: authors,
+            book: book
+        }
+        if (hasError) params.errorMessage = 'Error Creating Book'
+        res.render('books/new', params)
+    } catch {
+        res.redirect('/books')
+    }
+}
 module.exports = router
